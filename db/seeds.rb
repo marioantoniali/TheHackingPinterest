7707f3b3f56a1669a8b845c161646f91147581e9
Mario <mario.antoniali@gmail.com>
2018-07-26 03:56:45 +0200

    modif seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@ end
 
 #remplis la table pins (via la class) avec 20 faux noms et url, et aleatoirement des user_id  
 20.times do
-    pin = Pin.create(name: Faker::PrincessBride.character, URL: Faker::Internet.url, user_id: rand(1..10))
+    pin = Pin.create(name: Faker::PrincessBride.character, URL: Faker::Company.logo, user_id: rand(1..10))
   end
 
-#remplis la table comments (via la class) avec 15 faux contenus, et aleatoirement des user_id et category_id
+#remplis la table comments (via la class) avec 40 faux titres et corps de texte, et aleatoirement des user_id et pin_id
 40.times do
     comment = Comment.create(title: Faker::Hobbit.character, body: Faker::HeyArnold.quote, user_id: rand(1..10), pin_id: rand(1..20) )
 end
